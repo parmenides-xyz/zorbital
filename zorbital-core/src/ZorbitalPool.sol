5b91a675486c5a7f55ff7649b8fe2fa315715ed3
Daniel Yim <jedidiahcelloboy@hotmail.com>
2025-12-03 07:12:35 -0500

    Milestone 1

--- zorbital-core/src/ZorbitalPool.sol
+++ zorbital-core/src/ZorbitalPool.sol
@@ -1,27 +1,30 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.26;
 
+import "./libraries/Tick.sol";
+import "./libraries/Position.sol";
+
 contract ZorbitalPool {
     using Tick for mapping(int24 => Tick.Info);
     using Position for mapping(bytes32 => Position.Info);
     using Position for Position.Info;
 
-    int24 internal constant MIN_TICK = -887272;
-    int24 internal constant MAX_TICK = -MIN_TICK;
+    int24 internal constant MIN_TICK = 0;
+    int24 internal constant MAX_TICK = 4055;
 
-    // Pool tokens, immutable
-    address public immutable token0;
-    address public immutable token1;
+    // Pool tokens (n tokens)
+    address[] public tokens;
 
+    // Packing variables that are read together
     struct Slot0 {
-        // Current plane constant
-        uint160 alpha;
+        // Current sum of reserves
+        uint128 sumReserves;
         // Current tick
         int24 tick;
     }
     Slot0 public slot0;
 
-    // Analogue for liquidity, r.
+    // Consolidated radius, r (analogue of liquidity L)
     uint128 public r;
 
     // Ticks info
